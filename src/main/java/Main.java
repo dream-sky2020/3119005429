32de79b7b402c224f92c55c1ac7f13be48f5502e
3119005429陆梦龙 <872471288@qq.com>
2021-09-18 05:21:11 +0800

    解决了输出结果要四舍五入的问题

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,16 +1,15 @@
 
 import org.apache.commons.lang3.StringUtils;
 
-import java.io.Console;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
+import java.math.*;
 
 public class Main {
     public static void main(String[] args) {
         // 准备文件lol.txt其中的内容是AB
         File f = new File(args[0]);
         File f2=new File(args[1]);
+        File f3=new File(args[2]);
         char[] charter1;
         char[] charter2;
         String Charter1 = null;
@@ -38,5 +37,15 @@ public class Main {
         }
         double dis = StringUtils.getJaroWinklerDistance(Charter1, Charter2);
         System.out.println(dis);
+        try (FileWriter fr = new FileWriter(f3)) {
+            // 以字符流的形式把数据写入到文件中
+            BigDecimal disB=new BigDecimal(dis*100);
+            String data= new StringBuilder().append(String.valueOf(disB.setScale(2,RoundingMode.HALF_UP))).append("%").toString();
+            char[] cs = data.toCharArray();
+            fr.write(cs);
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
     }
 }
